be4bf0d9074e6ab4831efd3431527694b8540c55
Tolya <gorkov.anatoliy@yandex.ru>
2017-09-10 01:08:34 +0300

    deadline 15.09.17

--- 2/prog1.c
+++ 2/prog1.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdio_ext.h>
 #include <stdlib.h>
@@ -6,50 +5,50 @@
 
 int generateString(size_t n, char* string)
 {
-	if (string == NULL)
-		return 1;
-	size_t i;
-	*string = 'a';
-	for (i = 1; i < n; i++)
-	{
-		strncpy(string + (1 << i), string, (1 << i) - 1);
-		*(string + (1 << i) - 1) = 'a' + i;
-	}
-	*(string + (1 << n) - 1) = '\0';
+    if (string == NULL)
+        return 1;
+    size_t i;
+    *string = 'a';
+    for (i = 1; i < n; i++)
+    {
+        strncpy(string + (1 << i), string, (1 << i) - 1);
+        *(string + (1 << i) - 1) = 'a' + i;
+    }
+    *(string + (1 << n) - 1) = '\0';
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	size_t n;
-	int scanf_retval;
-	printf("Input n\n");
+    size_t n;
+    int scanf_retval;
+    printf("Input n\n");
 
-	scanf_retval = scanf("%lu", &n);
+    scanf_retval = scanf("%lu", &n);
 
-	while (scanf_retval != 1 || n > 26)
-	{
-		printf("Incorrect input. Please try again.\n");
-		if (scanf_retval != 1)
-			__fpurge(stdin);
-		scanf_retval = scanf("%lu", &n);
-	}
-	
-	char* string = malloc(sizeof(*string) * (1 << n));
-	if (string == NULL)
-	{
-		fprintf(stderr, "Error in memory allocation\n");
-		return 1;
-	}
-	if (generateString(n, string))
-	{
-		fprintf(stderr, "Error in string generation\n");
-		return 1;
-	}
+    while (scanf_retval != 1 || n > 26)
+    {
+        printf("Incorrect input. Please try again.\n");
+        if (scanf_retval != 1)
+            __fpurge(stdin);
+        scanf_retval = scanf("%lu", &n);
+    }
 
-	puts(string);
-	free(string);
-	return 0;
+    char* string = malloc(sizeof(*string) * (1 << n));
+    if (string == NULL)
+    {
+        fprintf(stderr, "Error in memory allocation\n");
+        return 1;
+    }
+    if (generateString(n, string))
+    {
+        fprintf(stderr, "Error in string generation\n");
+        return 1;
+    }
+
+    puts(string);
+    free(string);
+    return 0;
 }
 
