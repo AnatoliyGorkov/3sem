2197d7ce1ad767b6e7686dc1c1c1d4bceea76286
Tolya <gorkov.anatoliy@yandex.ru>
2017-09-19 08:28:26 +0300

    deadline 15.07.17 (fixed)

--- 2/split.h
+++ 2/split.h
@@ -1,49 +1,23 @@
 #ifndef SPLIT_USAGE
 
 #include <stdlib.h>
+#include <string.h>
 #define MAX_DELIM_SIZE 1000
 
-int delimetersContain(const char*, char, size_t);
 int split(char*, const char*, char**);
-
-int delimetersContain(const char* delimeters, char c, size_t n)    // tries no more than n delimeter characters
-{
-    if (delimeters == NULL)
-        return -1;
-    while (*delimeters != '\0' && *delimeters != c)
-        delimeters++;
-    if (*delimeters != '\0')
-        return 1;
-    else
-        return 0;
-}
 
 int split(char* string, const char* delimeters, char** tokens)
 {
-    if (string == NULL || delimeters == NULL || tokens == NULL)
-        return -1;
-    int i = 0, j = 0;
-
-    if (!delimetersContain(delimeters, *string, MAX_DELIM_SIZE))
-    {
-        *tokens = string;
-        j++;
-        i++;
-    }
-
-    while (string[i])
-    {
-        if (delimetersContain(delimeters, string[i], MAX_DELIM_SIZE))
-            string[i] = '\0';
-        else if (string[i - 1] == '\0')
-        {
-            tokens[j] = string + i;
-            j++;
-        }
-        i++;
-    }
-
-    return j;
+    char* saveptr;
+    char* curr_word;
+    curr_word = strtok_r(string, delimeters, &saveptr);
+    if (curr_word == NULL)
+        return 0;
+    *tokens = curr_word;
+    int i = 1;
+    while((curr_word = strtok_r(NULL, delimeters, &saveptr)) != NULL)
+        tokens[i++] = curr_word;
+    return i;
 }
 
 #define SPLIT_USAGE
